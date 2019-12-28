02c19e82c76f79dfd01089f36fe36c600a6bb7bb
Rene Juuse <rene.juuse@gmail.com>
2019-12-28 22:06:40 +0200

    Generic + keyof for @Dynamic

--- src/app/dynamic.decorator.ts
+++ src/app/dynamic.decorator.ts
@@ -3,7 +3,7 @@
   Meant to be used in conjunction with @Input()
 */
 // Can't export as a const lambda, AOT compilation would break.
-export function Dynamic(instanceCallbackName: string): any {
+export function Dynamic<T>(instanceCallbackName: keyof T): any {
   // Runs on the property when the container class prototype (not instance!)
   // is first loaded.
   return (target: Object, propName: string | symbol) => {
